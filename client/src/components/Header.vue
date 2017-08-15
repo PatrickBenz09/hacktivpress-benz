d46d773f2dd47dca88a1fc4477597b6e083279f8
Patrick Benz <patrickbenz.09@gmail.com>
2017-08-15 15:59:39 +0700

    Fourth Checkpoint

---
+++ client/src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <div class="nav-wrapper grey darken-4">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+    
+  }
+}
+</script>
+
+<style lang="css">
+</style>
